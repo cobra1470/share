599137a8b0cbfecd00801f1c3e27262cbf348b3d
Cobra Liu <lhg2work@sina.com>
2017-03-29 16:01:23 +0800

    Update jQuery-selector.js

--- common-Javascript/jQuery-selector.js
+++ common-Javascript/jQuery-selector.js
@@ -1,20 +1,20 @@
 
 #id	$("#lastname")	id="lastname" 的元素
-.class	$(".intro")	所有 class="intro" 的元素
-element	$("p")	所有 <p> 元素
+.class	            $(".intro")	所有 class="intro" 的元素
+element	           $("p")	所有 <p> 元素
  	 	 
-[attribute]	$("[href]")	所有带有 href 属性的元素
-[attribute=value]	$("[href='#']")	所有 href 属性的值等于 "#" 的元素
+[attribute]	       $("[href]")	所有带有 href 属性的元素
+[attribute=value]	 $("[href='#']")	所有 href 属性的值等于 "#" 的元素
 [attribute!=value]	$("[href!='#']")	所有 href 属性的值不等于 "#" 的元素
 [attribute$=value]	$("[href$='.jpg']")	所有 href 属性的值包含以 ".jpg" 结尾的元素
 
-:text	$(":text")	所有 type="text" 的 <input> 元素
-:password	$(":password")	所有 type="password" 的 <input> 元素
-:radio	$(":radio")	所有 type="radio" 的 <input> 元素
-:checkbox	$(":checkbox")	所有 type="checkbox" 的 <input> 元素
+:text	             $(":text")	所有 type="text" 的 <input> 元素
+:password         	$(":password")	所有 type="password" 的 <input> 元素
+:radio	            $(":radio")	所有 type="radio" 的 <input> 元素
+:checkbox	         $(":checkbox")	所有 type="checkbox" 的 <input> 元素
 
-:disabled	$(":disabled")	所有禁用的 input 元素
-:selected	$(":selected")	所有被选取的 input 元素
-:checked	$(":checked")	所有被选中的 input 元素
+:disabled	         $(":disabled")	所有禁用的 input 元素
+:selected	         $(":selected")	所有被选取的 input 元素
+:checked	          $(":checked")	所有被选中的 input 元素
 
 
